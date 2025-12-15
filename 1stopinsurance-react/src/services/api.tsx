ec62908c59c10329328dd9e43e9e6a497ca01c2c
nut2275 <nutthapon.sala@gmail.com>
2025-12-16 00:24:31 +0700

    add api service URI

--- 1stopinsurance-react/src/services/api.tsx
+++ 1stopinsurance-react/src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://onestopinsurance-react.onrender.com", // URL backend Node.js
+  baseURL: "https://onestopinsurance-react-1.onrender.com", // URL backend Node.js
   headers: {
     "Content-Type": "application/json",
   },
