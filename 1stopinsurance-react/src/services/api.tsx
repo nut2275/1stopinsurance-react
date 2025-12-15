f0f116a2bf23bb37d97b3001a687d7bbfd55110e
Annsan <air1180123@gmail.com>
2025-12-16 00:04:23 +0700

    addURL

--- 1stopinsurance-react/src/services/api.tsx
+++ 1stopinsurance-react/src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/", // URL backend Node.js
+  baseURL: "https://onestopinsurance-react.onrender.com", // URL backend Node.js
   headers: {
     "Content-Type": "application/json",
   },
